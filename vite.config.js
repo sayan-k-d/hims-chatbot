ba3b1af86765aa1acede53e9649c9e5f529b4764
sayan-k-d <sayankumar.d2000@gmail.com>
2025-12-03 01:11:22 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        widget: "widget.js", // 👈 MUST EXIST AT PROJECT ROOT
-      },
-      output: {
-        entryFileNames: "chatbot-widget.js", // 👈 final file
-        assetFileNames: "assets/[name][extname]",
-        chunkFileNames: "chunks/[name].js",
-        inlineDynamicImports: true,
-      },
-    },
-  },
+  // build: {
+  //   outDir: "dist",
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     input: {
+  //       widget: "widget.js", // 👈 MUST EXIST AT PROJECT ROOT
+  //     },
+  //     output: {
+  //       entryFileNames: "chatbot-widget.js", // 👈 final file
+  //       assetFileNames: "assets/[name][extname]",
+  //       chunkFileNames: "chunks/[name].js",
+  //       inlineDynamicImports: true,
+  //     },
+  //   },
+  // },
 });
