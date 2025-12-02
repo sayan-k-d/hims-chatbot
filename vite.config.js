c64a25d1a75299c6fb40722dfb2fe63bb281eb86
sayan-k-d <sayankumar.d2000@gmail.com>
2025-12-03 00:14:24 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        widget: "widget.js"   // important
-      },
-      output: {
-        entryFileNames: "chatbot-widget.js",   // no hashes
-        assetFileNames: "[name].[ext]",
-        chunkFileNames: "chunks/[name].js",
-        inlineDynamicImports: true
-      }
-    }
-  }
 });
